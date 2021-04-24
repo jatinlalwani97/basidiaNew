e26d8dc8677d3280489888063506b54be57c0768
jatin lalwani <jatinlalwani1997@gmail.com>
2021-04-25 00:49:40 +0530

    final commit 1

--- routes/post.js
+++ routes/post.js
@@ -5,27 +5,27 @@ const sequelize = require('../helpers/mysql');
 
 
 router.post('/',async function(req,res,next){
-  try{
-      const postBody = req.body;
-      
-      const post = await Post.create({
-          'user_id':postBody.user_id,
-          'post_body':postBody.post_body,
-          'post_img':postBody.post_img
-      })
-      return res.status(HttpCodes.OK).send({
-          'response': {
-            'message': 'post created',
-            'result': {
-              'post': post
-            }
-          },
-        });
-  }
-  catch (e) {
-      console.log(e.message);
-      next(e);
-  }
+    try{
+        const postBody = req.body;
+        
+        const post = await Post.create({
+            'user_id':postBody.user_id,
+            'post_body':postBody.post_body,
+            'post_img':postBody.post_img
+        })
+        return res.status(200).send({
+            'response': {
+              'message': 'post created',
+              'result': {
+                'post': post
+              }
+            },
+          });
+    }
+    catch (e) {
+        console.log(e.message);
+        next(e);
+    }
 });
 
 
@@ -37,7 +37,7 @@ router.delete('/:id',async function(req,res,next){
                 'id': postId,
               },
         })
-        return res.status(HttpCodes.OK).send({
+        return res.status(200).send({
             'response': {
               'message': 'post deleted successfully'
             },
